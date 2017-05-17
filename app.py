0c6454e6eb7c7e219aba2449b3e49399178d88fe
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-05-17 18:36:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ def webhook():
 url = "https://raw.githubusercontent.com/giacomo1989/prova-import/master/pizzaimport.json"
 response = urllib.request.urlopen(url)
 content = response.read()
+data = json.loads(content.decode("utf8"))
 #ok = data.get('price')
 #print data
 
@@ -49,7 +50,7 @@ def processRequest(req):
 			'prosciutto and funghi':6.00, 
 			'tonno and cipolla':6.90, 
 			'capricciosa':5.50}
-		speech = "ok 18.34,la pizza " +zone+ " costa "+str(cost[zone])+ " euro " 
+		speech = "ok 18.36,la pizza " +zone+ " costa "+str(cost[zone])+ " euro " 
 		res = makeWebhookResult(speech)
 		return res
 	
