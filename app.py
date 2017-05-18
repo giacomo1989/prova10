44a11fd610478058a0cd4720365231892e3e1415
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-05-18 11:57:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -48,13 +48,14 @@ def processRequest(req):
 		#bb = aa.get("margherita")
 		#bbc = aa.get(zone)
 		#aa = data.["menu"][0]["tipo"]
+		aa = data.get("menu")
 		
 		cost = {'margherita':3.50, 
 			'diavola':5.50, 
 			'prosciutto and funghi':6.00, 
 			'tonno and cipolla':6.90, 
 			'capricciosa':5.50}
-		speech = "ok 11.50,la pizza " +zone+ " costa "+str(cost[zone])+ " euro "+" questa prova che ho una "#+aa #+data.get("price") 
+		speech = "ok 11.57,la pizza " +zone+ " costa "+str(cost[zone])+ " euro "+" questa prova che ho una "#+aa #+data.get("price") 
 		res = makeWebhookResult(speech)
 		return res
 	
